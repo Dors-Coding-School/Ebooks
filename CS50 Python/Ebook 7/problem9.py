d5cc511b8a42c50000b77ab4d0cec07757a86b6b
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2022-08-05 16:55:46 -0700

    Create problem9.py

---
+++ CS50 Python/Ebook 7/problem9.py
@@ -0,0 +1,9 @@
+import re
+patterns = [ 'fox', 'dog', 'horse' ]
+text = 'The quick brown fox jumps over the lazy dog.'
+for pattern in patterns:
+    print('Searching for "%s" in "%s" ->' % (pattern, text),)
+    if re.search(pattern, text):
+        print('Matched!')
+    else:
+        print('Not Matched!')
