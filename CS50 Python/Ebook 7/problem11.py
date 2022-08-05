31e9d617303c3d314b62f8d78c4e3cb67cc514e8
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2022-08-05 16:56:21 -0700

    Create problem11.py

---
+++ CS50 Python/Ebook 7/problem11.py
@@ -0,0 +1,5 @@
+import re
+def extract_date(url):
+ return re.findall(r'/(\d{4})/(\d{1,2})/(\d{1,2})/', url)
+url1= "https://www.washingtonpost.com/news/football-insider/wp/2016/09/02/odellbeckhams-fame-rests-on-one-stupid-little-ball-josh-norman-tells-author/"
+print(extract_date(url1))
