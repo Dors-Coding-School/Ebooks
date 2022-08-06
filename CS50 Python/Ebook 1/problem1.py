8bba8bb001fd3fea43cc6c1f1b67d54b6532632f
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2022-08-05 17:09:51 -0700

    Create problem1.py

---
+++ CS50 Python/Ebook 1/problem1.py
@@ -0,0 +1,10 @@
+# Prompt user for x
+x = int(input("x: "))
+# Prompt user for y
+y = int(input ("y: "))
+if x < y:
+ print("x is less than y")
+elif x > y:
+ print("x is greater than y")
+else:
+ print("x is equal to y")
