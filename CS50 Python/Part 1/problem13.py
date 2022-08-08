b7d71fae774b4da0a97a522624136061649b75e3
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2022-08-08 10:26:51 -0700

    Create problem13.py

---
+++ CS50 Python/Part 1/problem13.py
@@ -0,0 +1,13 @@
+#Using 3 print statements
+print("cough")
+print("cough")
+print("cough")
+#Using a loop
+for i in range(3):
+    print("cough")
+#Using a function
+def cough(n):
+    for i in range(n):
+        print("cough")
+
+cough(3)
