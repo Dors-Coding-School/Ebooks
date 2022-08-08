38a449327206b86a9c1d9642c41ce19cc362a6c2
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2022-08-08 15:02:09 -0700

    Create problem1.py

---
+++ CS50/Fundamental Python/problem1.py
@@ -0,0 +1,11 @@
+from cs50 import get_int
+# Prompt user for x
+x = get_int("x: ")
+# Prompt user for y
+y = get_int("y: ")
+if x < y:
+ print("x is less than y")
+elif x > y:
+ print("x is greater than y")
+else:
+ print("x is equal to y")
