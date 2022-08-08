c6714d845c0fe35f88c8dd985c841aec0fb52ea4
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2022-08-08 14:56:58 -0700

    Create problem22.c

---
+++ CS50/Fundamental C Language/problem22.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+int test(int x, int y);
+int main(void)
+{
+ printf("%d\n",test(123, 456));
+ printf("%d\n",test(12, 512));
+ printf("%d\n",test(7, 87));
+ printf("%d\n",test(12, 45));
+} 
+int test(int x, int y)
+{
+ if (a + b >= 10 && a + b <= 20)
+ {
+ return 30;
+ }
+ else
+ {
+ return a + b;
+ }
+}
