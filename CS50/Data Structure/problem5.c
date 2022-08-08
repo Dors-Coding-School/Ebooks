a338b12b02c45c32c5a8259265d20626766dac09
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2022-08-08 12:27:48 -0700

    Create problem5.c

---
+++ CS50/Data Structure/problem5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Represents a node
+typedef struct node
+{
+ int number;
+ struct node *next;
+}
+node;
+void printLL(node * temp);
+int main(void)
+{
+ // List of size 0, initially not pointing to anything
+ node *list = NULL;
+ // Add number to list
+ node *n = malloc(sizeof(node));
+ n->number = 1;
+ n->next = NULL;
+ // We create our first node, store the value 1 in it, and leave the next
+ // pointer to point to nothing. Then, our list variable can point to it.
+ list = n;
+ // Add number to list
+ n = malloc(sizeof(node));
+ n->number = 2;
+ n->next = NULL;
+ 
+ // Now, we go our first node that list points to, and sets the next pointer
+ // on it to point to our new node, adding it to the end of the list:
+ list->next = n;
+ // Add number to list
+ n = malloc(sizeof(node));
+ n->number = 3;
+ n->next = NULL;
+ // We can follow multiple nodes with this syntax, using the next pointer
+ // over and over, to add our third new node to the end of the list:
+ list->next->next = n;
+ node *toDelete = list;
+ list = list->next;
+ free(toDelete);
+ node * temp = list;
+ while (temp != NULL)
+ {
+ printf("%d\n", temp->number);
+ temp = temp->next;
+ }
+}
