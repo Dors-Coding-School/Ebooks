e9ca0f33b5572c968febdfa1667b22794b6e2cf3
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2022-08-08 15:27:19 -0700

    Create problem30.py

---
+++ CS50/Fundamental Python/problem30.py
@@ -0,0 +1,13 @@
+text = "coding"
+
+#for each loop
+for char in text:
+ print(char)
+
+#printing index
+for index in range(len(text)):
+ print(index)
+
+#print the letters using the index with for loop
+for i in range(len(text)):
+ print(text[i])
