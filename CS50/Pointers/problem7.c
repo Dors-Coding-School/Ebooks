ec40e66e6175f953478f49136f2f5f2c1c438a7f
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2022-08-08 12:22:56 -0700

    Create problem7.c

---
+++ CS50/Pointers/problem7.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+   char *s = get_string("s: ");
+   char *t = malloc(strlen(s) + 1);
+   for (int i = 0, n = strlen(s); i < n + 1; i++)
+   {
+      t[i] = s[i];
+   }
+   t[0] = toupper(t[0]);
+   printf("s: %s\n", s);
+   printf("t: %s\n", t);
+}
