dcaccbeefca61fb5aecf31321db11d68d0fc7369
Dors Coding School <93161768+Dors-Coding-School@users.noreply.github.com>
2022-08-08 15:22:56 -0700

    Create problem20.py

---
+++ CS50/Fundamental Python/problem20.py
@@ -0,0 +1,14 @@
+x = [10, 9, 5, 3, 2]
+
+#for each loop
+for val in x:
+ print(val)
+
+#loop through the list index
+for i in range(len(x)):
+ print(i)
+
+#loop through the values of the list
+#using the index
+for i in range(len(x)):
+ print(x[i])
